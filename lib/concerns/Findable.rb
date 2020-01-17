1c1bfc90084a60d3512e68f0a8eae41dc0517a18
Romario Gustave <romagustave10@gmail.com>
2020-01-17 06:28:32 +0000

    Automatically backed up by Learn

--- lib/concerns/Findable.rb
+++ lib/concerns/Findable.rb
@@ -1,10 +1,12 @@
-module Concerns::Findable
-  def find_by_name(name)
-    self.all.detect {|e| e.name == name}
+module Concerns
+  
+  module Findable
+    def find_by_name(name)
+      all.detect{|a| a.name == name}
+    end
+    def find_or_create_by_name(name)
+      find_by_name(name) ? find_by_name(name) : create(name)
+    end
   end
-
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end
-
+  
 end
